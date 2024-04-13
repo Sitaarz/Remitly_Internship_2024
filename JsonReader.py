8b78a98b3d5116806adbb9d4af496d25ab3dacae
Krystian <krystiano1602@gmail.com>
2024-04-13 21:24:21 +0200

    Wildcard check feature implemented.

--- JsonReader.py
+++ JsonReader.py
@@ -2,34 +2,52 @@ import json
 import re
 from jsonschema import validate
 class AWS_JSON_Reader:
+    def __init__(self, JSON_schema_file_path:str)->None:
+        self.JSON_schema_path = JSON_schema_file_path
 
-    def check_policy_name(self, jsonDict: dict)->None:
-        if 'PolicyName' not in jsonDict.keys():
-            raise ValueError('No PolicyName attribute in JSON.')
-
-        name_pattern = '[\\w+=,.@-]{1,128}'
-        if not re.match(name_pattern, jsonDict['PolicyName']):
-            raise ValueError('PolicyName does not match pattern.')
-
-    def check_policyDocument(self, policyDict: dict):
-        pass
-
-    def verifie_json(self, file: str)->bool:
-        with open(file,'rb') as f:
+    def verify_JSON(self, path_to_AWS_file: str)->None:
+        with open(path_to_AWS_file,'rb') as f:
             try:
                 jsonDict = json.load(f)
-            except json.decoder.JSONDecodeError as e:
+            except json.decoder.JSONDecodeError:
                 print("EXCEPTION: Input file is not in JSON format.")
                 raise
+        with open(self.JSON_schema_path, 'rb') as f:
+            jsonSchema = json.load(f)
 
-        self.check_policy_name(jsonDict)
         try:
-            self.check_policyDocument(jsonDict['PolicyDocument'])
-        except KeyError:
-            print("No PolicyDocument attribute")
+            validate(jsonDict, jsonSchema) # jsonschema library function
+            print("JSON data is valid.")
+        except Exception as e:
+            print(f"JSON data is invalid: {e.message}")
             raise
 
 
+    def checkIfWildCardExists(self, path_to_AWS_file: str):
+        with open(path_to_AWS_file, 'rb') as f:
+            try:
+                policy_json_dict = json.load(f)
+            except json.decoder.JSONDecodeError:
+                print("EXCEPTION: Input file is not in JSON format.")
+                raise
+
+        statement = policy_json_dict['PolicyDocument']['Statement']
+
+        match statement:
+            case dict():
+                return statement['Resource'] == '*'
+            case list():
+                return any(s['Resource'] == '*' for s in statement)
+        return False
+
+    def verify_and_check_WildCard(self, path_to_AWS_file):
+        self.verify_JSON(path_to_AWS_file)
+        self.checkIfWildCardExists(path_to_AWS_file)
+
+
 if __name__ == '__main__':
-    reader = AWS_JSON_Reader()
-    reader.verifie_json("./data/test_1.json")
+    reader = AWS_JSON_Reader('./AWS_IAM_Role_Policy_Schema.json')
+    # reader.verify_JSON("./test_data/test_1.json")
+    # if reader.checkIfWildCardExists("./test_data/test_1.json"):
+    #     print("Resource in statement element equal *")
+    reader.verify_and_check_WildCard("./test_data/test_1.json")
